9aea9738ed8e9b7cb15d432719ed81b9173499a9
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-06-04 22:46:33 -0300

    init mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import Home from './src/pages/Home';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>
+  );
+}
